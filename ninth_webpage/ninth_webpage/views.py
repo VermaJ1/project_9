720c7abb44c54c58a2738b61c6f50bcaddd2bc8e
VermaJ1 <phaltukam2@gmail.com>
2025-08-12 22:10:54 +0530

    Add files via upload

---
+++ ninth_webpage/ninth_webpage/views.py
@@ -0,0 +1,6 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+def index(request):
+    return HttpResponse("Home Page")
+def home(request):
+    return render(request, 'index.html')
